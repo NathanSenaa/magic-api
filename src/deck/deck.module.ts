9d00e4fc55e9ddc8388b28baf40adc3df04b9cf2
NathanSenaa <128058745+NathanSenaa@users.noreply.github.com>
2024-09-07 20:26:58 -0300

    Update deck.module.ts

--- src/deck/deck.module.ts
+++ src/deck/deck.module.ts
@@ -3,7 +3,6 @@ import { DeckService } from './deck.service';
 import { DeckController } from './deck.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Deck, DeckSchema } from './deck.schema';
-
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Deck.name, schema: DeckSchema }]),
